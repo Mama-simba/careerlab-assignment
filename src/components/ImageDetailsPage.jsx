d00b3f2593d66b86339bbb9c9b40440dd5aef9ea
Cristina Padilla <68116083+Mama-simba@users.noreply.github.com>
2022-12-06 11:29:21 +0100

    Add back button

--- src/components/ImageDetailsPage.jsx
+++ src/components/ImageDetailsPage.jsx
@@ -1,6 +1,7 @@
-export default function ImageDetailsPage({ image, altText }) {
+export default function ImageDetailsPage({ image, altText, handleGoBack }) {
 	return (
 		<div className="ImageDetailsPage">
+			<button onClick={handleGoBack}>Back</button>
 			<img
 				alt={altText}
 				src={`https://www.artic.edu/iiif/2/${image}/full/843,/0/default.jpg`}
